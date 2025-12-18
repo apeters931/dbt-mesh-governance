74b41e0bd06dfb6485e9dc8c99d3515f411fb218
Alec Peters <alecpeters610@gmail.com>
2025-12-18 17:11:33 -0600

    add mesh governance project

---
+++ models/staging/stg_mesh__relationships.sql
@@ -0,0 +1,7 @@
+{% for source_schema in project_evaluator_source_location_prod %}
+
+  select * from {{ source_schema }}.int_all_dag_relationships
+
+  {% if not loop.last %} union all {% endif %}
+
+{% endfor %}
